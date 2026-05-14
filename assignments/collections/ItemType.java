42dbe6fd0564492a338047b982e5b2cc7b7b9201
ariunbaigali <Ariunbaigali9@gmail.com>
2026-05-15 07:23:04 +0800

    Implement collections - Ariunbaigali

--- assignments/collections/ItemType.java
+++ assignments/collections/ItemType.java
@@ -1,10 +1,5 @@
-// TODO: ItemType enum зарлана уу
-// - 4 утгатай: POTION, WEAPON, ARMOR, KEY
-//
-// public enum ItemType {
-//     POTION, WEAPON, ARMOR, KEY
-// }
 
-public enum ItemType {
-    // TODO
-}
+
+public enum  ItemType   {
+    POTION,  WEAPON,  ARMOR,  KEY
+ }
